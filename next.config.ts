f3b1eb971ec87474b62d142b571bb4c46766b9a9
Alex <delhui2014@hotmail.com>
2025-03-11 10:45:54 +0800

    off react eslint 2

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,6 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-    // suppress errors for missing 'import React' in files
-    "react/react-in-jsx-scope": "off",
-    // allow jsx syntax in js files (for next.js project)
-    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }], //should add ".ts" if typescript project
 };
 
 export default nextConfig;
